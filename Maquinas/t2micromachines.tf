6fb06c39f0cae52eb5dc4d3338ab979b51728305
dukercs <dukercs@gmail.com>
2019-12-20 12:45:26 -0300

    addsecurityandinterfaces

--- Maquinas/t2micromachines.tf
+++ Maquinas/t2micromachines.tf
@@ -5,32 +5,6 @@ resource "aws_instance" "iaasweek" {
   security_groups = ["${aws_securoty_group.allow_ssh.name}"]
 }
 
-
-resource "aws_key_pair" "main" {
-    key_name = "main"
-    public_key = "${file("%USERPROFILE%\\.ssh\\id_rsa.pub")}"
-  
-}
-
-
-resource "aws_security_group" "allow_ssh" {
-    name = "allow_ssh"
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = -1
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-  
-}
-
-
 output "iaasweek_public_dns" {
   value = "${aws_instance.iaasweek.public_dns}"
 }
