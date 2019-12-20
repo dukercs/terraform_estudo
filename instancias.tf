f7d50f0352d51569ec2542c3f5bcffe36b9f69a3
dukercs <dukercs@gmail.com>
2019-12-20 14:16:09 -0300

    AjustesTesteFinal

--- instancias.tf
+++ instancias.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "k8s_master" {
  instance_type   = "t2.micro"
  ami             = "ami-04b9e92b5572fa0d1"
  key_name        = "${aws_key_pair.main.key_name}"
@@ -14,7 +14,7 @@ resource "aws_instance" "web" {
 }
 
 
-resource "aws_instance" "web" {
+resource "aws_instance" "k8s_node1" {
  instance_type   = "t2.micro"
  ami             = "ami-04b9e92b5572fa0d1"
  key_name        = "${aws_key_pair.main.key_name}"
@@ -28,7 +28,7 @@ resource "aws_instance" "web" {
  }
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "k8s_node2" {
  instance_type   = "t2.micro"
  ami             = "ami-04b9e92b5572fa0d1"
  key_name        = "${aws_key_pair.main.key_name}"
