47bc308c0fea11039adf45f8feb610ffe8cd929f
Rodolpho Costa Stach <dukercs@gmail.com>
2021-05-12 16:24:22 -0300

    Criacao EC2 Basica testes WSL

---
+++ ec2/variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  description = "Definir qual a regiao"
+  default = "us-east-1"
+}
+
+variable "name" {
+  description = "Nome da aplicação"
+  default = "UbuntuServer01"
+}
+
+variable "env" {
+  description = "Ambiente"
+  default = "prod"
+}
+
+variable "ami" {
+  description = "Imagem a ser usada - Ubuntu Server 20.04"
+  default = "ami-054a31f1b3bf90920"
+}
+
+variable "instance_type" {
+  description = "Tipo de Instância AWS"
+  default = "t2.micro"
+}
