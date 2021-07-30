98bfecd668d87bd4a207df512c51649796f0ce62
Rodolpho Stach <dukercs@gmail.com>
2021-07-30 17:22:34 -0300

    Add port nomad

--- K8S/security.tf
+++ K8S/security.tf
@@ -19,6 +19,12 @@ resource "aws_security_group" "allow_ssh" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    ingress {
+        from_port = 4646
+        to_port = 4646
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     egress {
         from_port = 0
         to_port = 0
