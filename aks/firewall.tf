4f3d2731ef0d9e139037e552ade2d5c28cd18fe4
Rodolpho Costa Stach <dukercs@gmail.com>
2022-07-22 14:58:20 -0300

    Ajustado mesmo RG

--- aks/firewall.tf
+++ aks/firewall.tf
@@ -1,12 +1,7 @@
-resource "azurerm_resource_group" "wordpressrg" {
-  name     = "wordpressrg"
-  location = "westus3"
-}
- 
 resource "azurerm_network_security_group" "wordpressnsg" {
   name                = "wordpresstest"
   location            = "westus3"
-  resource_group_name = azurerm_resource_group.wordpressrg.name
+  resource_group_name = azurerm_resource_group.default.name
  
 }
  
@@ -21,6 +16,6 @@ resource "azurerm_network_security_rule" "testrules" {
   destination_port_range      = each.value.destination_port_range
   source_address_prefix       = each.value.source_address_prefix
   destination_address_prefix  = each.value.destination_address_prefix
-  resource_group_name         = azurerm_resource_group.wordpressrg.name
+  resource_group_name         = azurerm_resource_group.default.name
   network_security_group_name = azurerm_network_security_group.wordpressnsg.name
 }
